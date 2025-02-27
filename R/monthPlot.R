0ea372c7994affa7b0d03087c46c9c31af897e68
Mitsuo Shiota <mitsuoxv@gmail.com>
2025-02-27 13:22:10 +0900

    update January 2025 trade

--- R/monthPlot.R
+++ R/monthPlot.R
@@ -4,7 +4,7 @@ monthPlotUI <- function(id) {
       column(4,
              sliderInput(NS(id, "year_range"),
                          label = h4("Select year range"),
-                         min = 2009, max = 2024, value = c(2011, 2024),
+                         min = 2009, max = 2025, value = c(2011, 2025),
                          sep = "")
       ),
     ),
--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Mitsuo Shiota
 [![R-CMD-check](https://github.com/mitsuoxv/jp-trade/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/mitsuoxv/jp-trade/actions/workflows/R-CMD-check.yaml)
 <!-- badges: end -->
 
-Updated: 2025-01-30
+Updated: 2025-02-27
 
 ## Summary
 
